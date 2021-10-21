ce13bde4d0992ab76e5f4e4d627cd65482a6548e
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2021-10-20 21:51:11 -0300

    feat: routes msg

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controller/AuthenticateUserController";
+import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
 
 router.post("/authenticate", new AuthenticateUserController().handle);
+
+router.post(
+  "/messages",
+  ensureAuthenticated,
+  new AuthenticateUserController().handle
+);
 
 export { router };
